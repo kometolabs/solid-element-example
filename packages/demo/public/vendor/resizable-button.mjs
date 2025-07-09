c0fb06089bc157c5c953c5dabbe753522172734f
Konstantin Komelin <konstantin.komelin@gmail.com>
2025-07-09 17:47:51 +0200

    Improved building process for the demo package.

---
+++ packages/demo/public/vendor/resizable-button.mjs
@@ -0,0 +1,1 @@
+"A workaround to make Vite build the project and then copy resizable-button.mjs to the dist folder."
--- packages/demo/vite.config.js
+++ packages/demo/vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/resizable-button/dist/resizable-button.mjs',
+          dest: 'vendor',
+        },
+      ],
+    }),
+  ],
   server: {
     open: '/index.html',
   },
